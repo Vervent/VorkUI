92c398370a53ff400a8f8b159d7b104f54336045
vervent@gmail.com <LF31-*25>
2020-12-29 18:17:05 +0100

    player GUI now manage properly positionning frame start of submodule management

--- Config/GUI/Modules/UnitFrames/Units/player.lua
+++ Config/GUI/Modules/UnitFrames/Units/player.lua
@@ -2,6 +2,21 @@ local V = select(2, ...):unpack()
 
 local Install = V.Install
 
+local borderColor = { 1, 1, 1, 0.2 }
+local borderSize = 1
+
+local dropdownAnchorData = {
+    { text = 'CENTER' },
+    { text = 'TOPLEFT' },
+    { text = 'TOP' },
+    { text = 'TOPRIGHT' },
+    { text = 'RIGHT' },
+    { text = 'BOTTOMRIGHT' },
+    { text = 'BOTTOM' },
+    { text = 'BOTTOMLEFT' },
+    { text = 'LEFT' }
+}
+
 local scrollButtonClick = function(self)
     local container = self:GetParent()
     local parent = container:GetParent()
@@ -84,9 +99,9 @@ local generalEntry = {
             type = 'empty',
             params = {
                 name = 'PlayerFrameSize',
-                size = { 210, 90},
+                size = { 200, 90},
                 point = { 'TOPLEFT', 0, -30 },
-                border = { 1, { 1, 1, 1, 0.2 } },
+                border = { borderSize, borderColor },
             },
             widgets = {
                 {
@@ -98,63 +113,173 @@ local generalEntry = {
                     data = {
                         'OVERLAY',
                         nil,
-                        'Frame Options'
+                        'Frame Size'
                     }
-                },
+                }, --SECTION LABEL
                 {
                     type = 'label',
                     params = {
                         size = { 100, 25 },
-                        point = { 'TOPLEFT', 0, -30 }
+                        point = { 'TOPLEFT', 0, -15 }
                     },
                     data = {
                         'OVERLAY',
                         'GameFontNormal',
                         'Width'
                     }
-                },
+                }, --WIDTH LABEL
                 {
                     type = 'editbox',
                     params = {
                         size = { 50, 25 },
-                        point = { 'TOPLEFT', 110, -30 },
+                        point = { 'TOPLEFT', 110, -15 },
                         template = 'NumericInputSpinnerTemplate',
                         dboption = getPath('Size', 1)
                     },
                     data = { nil, nil, nil, {0, 500} }
-                },
+                }, --WIDTH EDIT
                 {
                     type = 'label',
                     params = {
                         size = { 100, 25 },
-                        point = { 'TOPLEFT', 0, -60 }
+                        point = { 'TOPLEFT', 0, -45 }
                     },
                     data = {
                         'OVERLAY',
                         'GameFontNormal',
                         'Height'
                     }
-                },
+                }, --HEIGHT LABEL
                 {
                     type = 'editbox',
                     params = {
                         size = { 50, 25 },
-                        point = { 'TOPLEFT', 110, -60 },
+                        point = { 'TOPLEFT', 110, -45 },
                         template = 'NumericInputSpinnerTemplate',
                         dboption = getPath('Size', 2)
                     },
                     data = { nil, nil, nil, {0, 500} }
-                },
+                }, --HEIGHT EDIT
             },
             childs = {}
-        },
+        }, --SUBFRAME SIZE
+        {
+            type = 'empty',
+            params = {
+                name = 'PlayerFramePosition',
+                size = { 435, 90},
+                point = { 'TOPLEFT', 210, -30 },
+                border = { borderSize, borderColor },
+            }, --SUBFRAME PARAMS
+            widgets = {
+                {
+                    type = 'label',
+                    params = {
+                        size = { 400, 30 },
+                        point = { 'TOP', 0, 15 }
+                    },
+                    data = {
+                        'OVERLAY',
+                        nil,
+                        'Frame Position'
+                    }
+                }, --SECTION LABEL
+                {
+                    type = 'label',
+                    params = {
+                        size = { 105, 25 },
+                        point = { 'TOPLEFT', 10, -15 }
+                    },
+                    data = {
+                        'OVERLAY',
+                        'GameFontNormal',
+                        'Local Anchor'
+                    }
+                }, --LOCAL ANCHOR LABEL
+                {
+                    type = 'dropdownmenu',
+                    params = {
+                        size = { 100, 25 },
+                        point = { 'TOPLEFT', 110, -15 },
+                        dboption = getPath('Point', 1)
+                    },
+                    data = dropdownAnchorData,
+                }, --LOCAL ANCHOR DROPDOWN
+                {
+                    type = 'label',
+                    params = {
+                        size = { 105, 25 },
+                        point = { 'TOPLEFT', 10, -45 }
+                    },
+                    data = {
+                        'OVERLAY',
+                        'GameFontNormal',
+                        'Parent Anchor'
+                    }
+                }, --PARENT ANCHOR LABEL
+                {
+                    type = 'dropdownmenu',
+                    params = {
+                        size = { 100, 25 },
+                        point = { 'TOPLEFT', 110, -45 },
+                        dboption = getPath('Point', 3)
+                    },
+                    data = dropdownAnchorData,
+                }, --PARENT ANCHOR DROPDOWN
+                { --X-OFFSET
+                    type = 'label',
+                    params = {
+                        size = { 100, 25 },
+                        point = { 'TOPLEFT', 235, -15 }
+                    },
+                    data = {
+                        'OVERLAY',
+                        'GameFontNormal',
+                        'X-offset'
+                    }
+                }, --X-OFFSET LABEL
+                {
+                    type = 'editbox',
+                    params = {
+                        size = { 50, 25 },
+                        point = { 'TOPLEFT', 345, -15 },
+                        template = 'NumericInputSpinnerTemplate',
+                        dboption = getPath('Point', 4)
+                    },
+                    data = { nil, nil, nil, {-250, 250} }
+                }, --X-OFFSET EDITBOX
+                {
+                    type = 'label',
+                    params = {
+                        size = { 100, 25 },
+                        point = { 'TOPLEFT', 235, -45 }
+                    },
+                    data = {
+                        'OVERLAY',
+                        'GameFontNormal',
+                        'Y-offset'
+                    }
+                }, --Y-OFFSET LABEL
+                {
+                    type = 'editbox',
+                    params = {
+                        size = { 50, 25 },
+                        point = { 'TOPLEFT', 345, -45 },
+                        template = 'NumericInputSpinnerTemplate',
+                        dboption = getPath('Point', 5)
+                    },
+                    data = { nil, nil, nil, {-250, 250} }
+                }, --Y-OFFSET EDITBOX
+            },
+            childs = {}
+        }, -- SUBFRAME POSITION
         {
             type = 'empty',
             params = {
                 name = 'PlayerSubmodule',
-                size = { 600, 250},
+                size = { 645, 250},
                 point = { 'TOPLEFT', 0, -132 },
-                border = { 1, { 1, 1, 1, 0.2 } },
+                border = { borderSize, borderColor },
                 updateWidgetsLayout = 2,
             },
             widgets = {
@@ -279,20 +404,19 @@ local function addCheckBox(parent, name)
             size = { 25, 25 },
             point = { "TOPLEFT", 2, -2 },
             template = 'UICheckButtonTemplate',
+            dboption = getPath(name..'Submodule')
         },
         data = { name },
     }
 
     tinsert(parent, box)
 end
-
 local function addCheckboxTo(entry, dataTable)
     for idx = 1, #dataTable do
         addCheckBox(entry.widgets, dataTable[idx])
     end
 
 end
-
 local function addEntryToScroll(name, entry)
     local scrollItem = {
         type = 'widget',
@@ -312,7 +436,7 @@ local function addEntryToScroll(name, entry)
 end
 
 addEntryToScroll('General', generalEntry)
-addCheckboxTo(generalEntry.childs[2], subModules)
+addCheckboxTo(generalEntry.childs[3], subModules)
 
 addEntryToScroll('Fonts', healthEntry)
 addEntryToScroll('Health', healthEntry)
