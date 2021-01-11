57a951996d8b8a63bd841a7605360a0868b58aa5
Florian Guilleminot <vervent@gmail.com>
2021-01-11 15:57:23 +0100

    Container can now be movable and clamped to screen

--- Libs/LibGUI/LibGUI.lua
+++ Libs/LibGUI/LibGUI.lua
@@ -74,6 +74,25 @@ function LibGUI:BindScript(container, event, fct)
 end
 
 --[[
+    This function bind a script handler to the container
+        container to handle the script
+        event the script event to handle ( 'OnEvent', 'OnClick', ...), take care to bind a supported script
+        fct the callback to execute
+]]--
+function LibGUI:SetMovableContainer(container)
+    if isValidContainer(container) and containers[container.type].Bind then
+        container:SetMovable(true)
+        container:EnableMouse(true)
+        container:RegisterForDrag("LeftButton")
+        container:SetClampedToScreen(true)
+        containers[container.type].Bind(container, 'OnDragStart', container.StartMoving)
+        containers[container.type].Bind(container, 'OnDragStop', container.StopMovingOrSizing)
+    else
+        print ("INVALID CONTAINER, CANNOT BE MOVABLE ")
+    end
+end
+
+--[[
     This function create a new container
         t type of container
         parent the parent of the container. The parent can be UIParent, nil, or Another Frame. If
