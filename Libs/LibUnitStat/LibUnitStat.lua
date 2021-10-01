edce943275e4b1e19af6c2a5d3623d38911fe105
Guilleminot Florian <f.guilleminot@isartdigital.com>
2021-10-01 16:11:51 +0200

    Libs :  - Add LibUnitStat to encapsulate the player stats data

---
+++ Libs/LibUnitStat/LibUnitStat.lua
@@ -0,0 +1,510 @@
+--[[
+	local LibUnitStat = LibStub:GetLibrary("LibUnitStat")
+	if (not LibUnitStat) then return end
+
+	LibUnitStat:AddStats('STRENGTH', 'STAMINA')
+	LibUnitStat:AddStat('INTELLECT')
+	LibUnitStat:RemoveStat('STRENGTH')
+	LibUnitStat:RemoveStats('INTELLECT')
+	LibUnitStat:ClearStats()
+
+	LibUnitStat:GetStat('STAMINA')
+	LibUnitStat:GetStat('stamina')
+	LibUnitStat:GetAllStats()
+
+    LibUnitStat:Enable()
+    LibUnitStat:Disable()
+--]]
+
+local LibUnitStat = LibStub:NewLibrary("LibUnitStat", 1)
+
+if not LibUnitStat then
+    return
+end
+
+local frame = CreateFrame('Frame')
+
+local function getStats(fct, ratingKey)
+    local value, coeff = fct()
+    local rating, percent
+    if ratingKey ~= nil then
+        rating = GetCombatRating(ratingKey)
+        percent = GetCombatRatingBonus(ratingKey)
+    end
+    return value, coeff, rating, percent
+end
+
+local function getPrimaryStats(unit, id)
+    if (unit ~= 'player') then
+        return ;
+    end
+
+    return UnitStat(unit, id)
+end
+
+local stats = {}
+local StatGetter = {
+    -- General
+    ["HEALTH"] = {
+        Get = function(unit)
+            if (not unit) then
+                unit = 'player'
+            end
+            return UnitHealthMax(unit)
+        end
+    },
+    ["POWER"] = {
+        Get = function(unit)
+            if (not unit) then
+                unit = 'player'
+            end
+            local powerType, powerToken = UnitPowerType(unit)
+            local power = UnitPowerMax(unit) or 0;
+            return power, powerType, powerToken
+        end
+    },
+    ["ALTERNATE_MANA"] = {
+        Get = function(unit)
+            if (not unit) then
+                unit = 'player'
+            end
+            local _, class = UnitClass(unit);
+            if (class ~= "DRUID" and (class ~= "MONK" or GetSpecialization() ~= SPEC_MONK_MISTWEAVER)) then
+                return ;
+            end
+            local powerType, powerToken = UnitPowerType(unit);
+            if (powerToken == "MANA") then
+                return ;
+            end
+
+            local power = UnitPowerMax(unit, 0);
+            return power, powerType, powerToken
+        end
+    },
+    ["ITEM_LEVEL"] = {
+        Get = function(unit)
+            if (unit ~= 'player') then
+                return
+            end
+
+            local avgItemLevel, avgItemLevelEquipped, avgItemLevelPvP = GetAverageItemLevel()
+            local minItemLevel = C_PaperDollInfo.GetMinItemLevel()
+
+            return avgItemLevel, avgItemLevelEquipped, avgItemLevelPvP, minItemLevel
+        end
+    },
+    ["MOVE_SPEED"] = {
+        Get = function(unit)
+            if (unit ~= 'player') then
+                return ;
+            end
+
+            local currentSpeed, runSpeed, flightSpeed, swimSpeed = GetUnitSpeed(unit);
+            --local baseSpeed = 7
+            --local wasSwimming = nil
+            --runSpeed = runSpeed/baseSpeed*100;
+            --flightSpeed = flightSpeed/baseSpeed*100;
+            --swimSpeed = swimSpeed/baseSpeed*100;
+            --currentSpeed = currentSpeed/baseSpeed*100;
+
+            ---- Determine whether to display running, flying, or swimming speed
+            --local speed = runSpeed;
+            --local swimming = IsSwimming(unit);
+            --if (swimming) then
+            --    speed = swimSpeed;
+            --elseif (IsFlying(unit)) then
+            --    speed = flightSpeed;
+            --end
+            --
+            ---- Hack so that your speed doesn't appear to change when jumping out of the water
+            --if (IsFalling(unit)) then
+            --    if (wasSwimming) then
+            --        speed = swimSpeed;
+            --    end
+            --else
+            --    wasSwimming = swimming;
+            --end
+
+            --Let the logic for the user
+            return currentSpeed, runSpeed, flightSpeed, swimSpeed
+
+        end
+    },
+
+    -- Base stats
+    ["STRENGTH"] = {
+        Get = function(unit)
+            return getPrimaryStats(unit, 1)
+        end
+    },
+    ["AGILITY"] = {
+        Get = function(unit)
+            return getPrimaryStats(unit, 2)
+        end
+    },
+    ["INTELLECT"] = {
+        Get = function(unit)
+            return getPrimaryStats(unit, 4)
+        end
+    },
+    ["STAMINA"] = {
+        Get = function(unit)
+            return getPrimaryStats(unit, 3)
+        end
+    },
+
+    -- Enhancements
+    ["CRIT_CHANCE"] = {
+        Get = function(unit)
+            if (unit ~= 'player') then
+                return ;
+            end
+
+            local spellCrit = {}
+            for i = 1, MAX_SPELL_SCHOOLS do
+                spellCrit[i] = GetSpellCritChance(i)
+            end
+
+            local rangedCrit = GetRangedCritChance();
+            local meleeCrit = GetCritChance();
+
+            return meleeCrit, GetCombatRating(9), GetCombatRatingBonus(9),
+            rangedCrit, GetCombatRating(10), GetCombatRatingBonus(10),
+            unpack(spellCrit), GetCombatRating(11), GetCombatRatingBonus(11)
+        end
+    },
+    ["HASTE"] = {
+        Get = getStats(GetHaste, 18),
+    },
+    ["MASTERY"] = {
+        Get = getStats(GetMasteryEffect, 26),
+    },
+    ["VERSATILITY"] = {
+        Get = function()
+            local versatility = GetCombatRating(28);
+            local outcomeRatingBonus = GetCombatRating(28)
+            local outcomeVersaBonus = GetVersatilityBonus(28)
+            local incomeRatingBonus = GetCombatRating(29)
+            local incomeVersaBonus = GetVersatilityBonus(29)
+            return versatility, outcomeRatingBonus, outcomeVersaBonus, incomeRatingBonus, incomeVersaBonus
+        end
+    },
+    ["LIFESTEAL"] = {
+        Get = getStats(GetLifesteal, 17),
+    },
+    ["AVOIDANCE"] = {
+        Get = getStats(GetAvoidance, 21),
+    },
+    ["SPEED"] = {
+        Get = getStats(GetSpeed, 14)
+    },
+
+    -- Attack
+    ["ATTACK_DAMAGE"] = {
+        Get = function(unit)
+            local _, offhandSpeed = UnitAttackSpeed(unit)
+            local minDamage, maxDamage, minOffHandDamage, maxOffHandDamage, physicalBonusPos, physicalBonusNeg, percent = GetAppropriateDamage(unit)
+
+            -- calculate base damage
+            minDamage = (minDamage / percent) - physicalBonusPos - physicalBonusNeg
+            maxDamage = (maxDamage / percent) - physicalBonusPos - physicalBonusNeg
+
+            local baseDamage = (minDamage + maxDamage) * 0.5
+            local fullDamage = (baseDamage + physicalBonusPos + physicalBonusNeg) * percent
+
+            -- If there's an offhand speed then add the offhand info to the tooltip
+            if (offhandSpeed and minOffHandDamage and maxOffHandDamage) then
+                minOffHandDamage = (minOffHandDamage / percent) - physicalBonusPos - physicalBonusNeg
+                maxOffHandDamage = (maxOffHandDamage / percent) - physicalBonusPos - physicalBonusNeg
+
+                local offhandBaseDamage = (minOffHandDamage + maxOffHandDamage) * 0.5
+                local offhandFullDamage = (offhandBaseDamage + physicalBonusPos + physicalBonusNeg) * percent
+
+                return baseDamage, fullDamage, offhandBaseDamage, offhandFullDamage
+            end
+
+            return baseDamage, fullDamage
+
+        end
+    },
+    ["SPELL_POWER"] = {
+        Get = function(_, school)
+            local spellPower = {}
+
+            if school then
+                if school > 0 and school < MAX_SPELL_SCHOOLS then
+                    return GetSpellBonusDamage(school)
+                elseif school == MAX_SPELL_SCHOOLS then
+                    return GetSpellBonusHealing()
+                end
+            end
+
+            for i = 1, MAX_SPELL_SCHOOLS do
+                spellPower[i] = GetSpellBonusDamage(i)
+            end
+            spellPower[MAX_SPELL_SCHOOLS] = GetSpellBonusHealing()
+
+            return unpack(spellPower)
+        end
+    },
+    ["ATTACK_POWER"] = {
+        Get = function(unit)
+            local base, posBuff, negBuff = UnitAttackPower(unit)
+
+            return base, posBuff, negBuff
+        end
+    },
+    ["RANGED_ATTACK_POWER"] = {
+        Get = function(unit)
+            if not IsRangedWeapon() then
+                return
+            end
+            local base, posBuff, negBuff = UnitRangedAttackPower(unit);
+
+            return base, posBuff, negBuff
+        end
+    },
+    ["ATTACK_ATTACKSPEED"] = {
+        Get = function(unit)
+            local meleeHaste = GetMeleeHaste();
+            local speed, offhandSpeed = UnitAttackSpeed(unit);
+           return meleeHaste, speed, offhandSpeed
+        end
+    },
+    ["ENERGY_REGEN"] = {
+        Get = function(unit)
+            if ( unit ~= 'player' ) then
+                return
+            end
+
+            local _, powerToken = UnitPowerType(unit);
+            if (powerToken ~= "ENERGY") then
+                return
+            end
+
+            local regenRate = GetPowerRegen();
+            return regenRate
+        end
+    },
+    ["RUNE_REGEN"] = {
+        Get = function(unit)
+            if ( unit ~= 'player' ) then
+                return
+            end
+
+            local _, class = UnitClass(unit)
+            if (class ~= "DEATHKNIGHT") then
+                return
+            end
+
+            local _, regenRate = GetRuneCooldown(1) -- Assuming they are all the same for now
+
+            return regenRate
+        end
+    },
+    ["FOCUS_REGEN"] = {
+        Get = function(unit)
+            if ( unit ~= 'player' ) then
+                return;
+            end
+
+            local _, powerToken = UnitPowerType(unit)
+            if (powerToken ~= "FOCUS") then
+                return;
+            end
+
+            local regenRate = GetPowerRegen()
+
+            return regenRate
+        end
+    },
+
+    -- Spell
+    ["MANA_REGEN"] = {
+        Get = function(unit)
+            if ( unit ~= 'player' ) then
+                return
+            end
+
+            if ( not UnitHasMana(unit) ) then
+                return
+            end
+
+            local base, combat = GetManaRegen();
+            -- All mana regen stats are displayed as mana/5 sec.
+            base = floor(base * 5.0);
+            combat = floor(combat * 5.0);
+
+            return base, combat
+        end
+    },
+
+    -- Defense
+    ["ARMOR"] = {
+        Get = function(unit)
+            local baselineArmor, effectiveArmor, armor, bonusArmor = UnitArmor(unit)
+
+            local armorReduction = C_PaperDollInfo.GetArmorEffectiveness(effectiveArmor, UnitEffectiveLevel(unit))*100
+            local armorReductionAgainstTarget = (C_PaperDollInfo.GetArmorEffectivenessAgainstTarget(effectiveArmor) or 0)*100
+
+            return baselineArmor, effectiveArmor, armor, bonusArmor, armorReduction, armorReductionAgainstTarget
+        end
+    },
+    ["DODGE"] = {
+        Get = function(unit)
+            if (unit ~= 'player') then
+                return;
+            end
+
+            local chance = GetDodgeChance()
+            return chance
+        end
+    },
+    ["PARRY"] = {
+        Get = function(unit)
+            if (unit ~= 'player') then
+                return;
+            end
+
+            local chance = GetParryChance();
+            return chance
+        end
+    },
+    ["BLOCK"] = {
+        Get = function(unit)
+            if (unit ~= 'player') then
+                return
+            end
+
+            local chance = GetBlockChance()
+            local shieldBlockArmor = GetShieldBlock()
+            local blockArmorReduction = C_PaperDollInfo.GetArmorEffectiveness(shieldBlockArmor, UnitEffectiveLevel(unit))*100
+            local blockArmorReductionAgainstTarget = (C_PaperDollInfo.GetArmorEffectivenessAgainstTarget(shieldBlockArmor) or 0)*100
+
+            return chance, shieldBlockArmor, blockArmorReduction, blockArmorReductionAgainstTarget
+        end
+    },
+    ["STAGGER"] = {
+        Get = function(unit)
+            local stagger, staggerAgainstTarget = C_PaperDollInfo.GetStaggerPercentage(unit);
+
+            return stagger, staggerAgainstTarget
+        end
+    },
+}
+
+--
+local events = {
+    'CHARACTER_POINTS_CHANGED',
+    'UNIT_STATS',
+    'UNIT_RANGEDDAMAGE',
+    'UNIT_ATTACK_POWER',
+    'UNIT_RANGED_ATTACK_POWER',
+    'UNIT_ATTACK',
+    'UNIT_SPELL_HASTE',
+    'UNIT_RESISTANCES',
+    'PLAYER_GUILD_UPDATE',
+    'SKILL_LINES_CHANGED',
+    'COMBAT_RATING_UPDATE',
+    'MASTERY_UPDATE',
+    'SPEED_UPDATE',
+    'LIFESTEAL_UPDATE',
+    'AVOIDANCE_UPDATE',
+    'PLAYER_TALENT_UPDATE',
+    'BAG_UPDATE',
+    'PLAYER_EQUIPMENT_CHANGED',
+    'PLAYER_AVG_ITEM_LEVEL_UPDATE',
+    'PLAYER_DAMAGE_DONE_MODS',
+    'ACTIVE_TALENT_GROUP_CHANGED',
+    'SPELL_POWER_CHANGED',
+    'CHARACTER_ITEM_FIXUP_NOTIFICATION',
+    'PLAYER_TARGET_CHANGED'
+}
+local unitEvents = {
+    'UNIT_DAMAGE',
+    'UNIT_ATTACK_SPEED',
+    'UNIT_MAXHEALTH',
+    'UNIT_AURA',
+}
+
+local StatObserved = {}
+
+---@param stat string the statistic to track
+function LibUnitStat:AddStat(stat)
+    local s = strupper(stat)
+
+    if not StatObserved[s] then
+        StatObserved[s] = {}
+    end
+
+end
+
+---@param stat string the statistic to remove
+function LibUnitStat:RemoveStat(stat)
+    local s = strupper(stat)
+
+    if StatObserved[s] then
+        StatObserved[s] = nil
+    end
+end
+
+---@vararg string Statistics to remove
+function LibUnitStat:RemoveStats(...)
+    for i = 1, select('#', ...) do
+        self:RemoveStat(select(i, ...))
+    end
+end
+
+---@vararg string Statistics to track
+function LibUnitStat:AddStats(...)
+    for i = 1, select('#', ...) do
+        self:AddStat(select(i, ...))
+    end
+end
+
+---wipe the tracked stat table
+function LibUnitStat:ClearStats()
+    wipe(StatObserved)
+end
+
+local function onEvent(_, unit,...)
+
+    for stat, v in pairs(StatObserved) do
+        v = { StatGetter[stat].Get(unit or 'player') }
+    end
+
+end
+
+---Enable the module
+function LibUnitStat:Enable()
+
+    for _, e in ipairs(events) do
+        frame:RegisterEvent(e)
+    end
+
+    for _, e in ipairs(unitEvents) do
+        frame:RegisterUnitEvent(e, 'player')
+    end
+
+    frame:SetScript('OnEvent', onEvent)
+end
+
+---Disable the module
+function LibUnitStat:Disable()
+    frame:UnregisterAllEvents()
+end
+
+---@param statName string the statistic to get
+---@return number[]|nil may return multiples values or nil
+function LibUnitStat:GetStat( statName )
+    if (not statName) or (not stats[statName]) then
+        return
+    end
+
+    return stats[statName]
+end
+
+---@return table<string, number[]> return an array of value for every stat
+function LibUnitStat:GetAllStats()
+    return stats
+end
